abfbc835707d52305e9e50815905e2aa34fc49cb
Samuel Luciano <108229765+SamuelL10Br@users.noreply.github.com>
2025-12-17 19:09:19 -0300

    Variable, Types, Input
    The process of coding in python is variables, numbers type's and input.

---
+++ Python/aula1.py
@@ -0,0 +1,36 @@
+#Variável e Tipos
+idade = 25
+nome = "Samuel"
+altura = 1.78
+estudas = True
+
+#Saídas
+print(idade)
+print(nome)
+print(altura)
+print(estudas)
+
+#Operações básicas
+a = 10
+b = 80
+print("Adição:", a + b)
+print("Multiplicação:", a * b)
+print("Subtração:", a - b)
+print("Divisão:", a / b)
+print("Divisão por inteiro:", a // b)
+print("Exponencial:", a ** b)
+print("Modulo:", a % b)
+
+#Declarando nome, curso e idade em uma variável e trazendo o retorno disto.
+nome = "Samuel"
+idade = 22
+curso = "Ciência de dados & Segurança"
+
+print("Meu nome é",nome, "tenho",idade,"anos e estudo",curso)
+
+#Pedindo ao usuário digitos para calcular e trazer um resultado como saída
+valor = int(input("Digite um valor:"))
+valor2 = int(input("Digite mais um:"))
+resultado = valor + valor2
+valorfin = resultado
+print("O resultado de:",valor,"+",valor2,"igual a:",valorfin)
