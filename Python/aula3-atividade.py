0c2028cf7584a810ddd747e413840a47beeefafd
Samuel Luciano <108229765+SamuelL10Br@users.noreply.github.com>
2025-12-19 22:30:06 -0300

    Update aula3-atividade.py

--- Python/aula3-atividade.py
+++ Python/aula3-atividade.py
@@ -5,10 +5,10 @@ while contador<=10:
 # Aqui o contador em (while), fez a contagem de 1 até 10
 
 for contagem in range(1, 11):
-    if contagem <=10:
         print(contagem)
 # Usando (for) em range para contar de 1 até 10 
 
 for pares in range(1,21):
     if pares % 2 == 0:
         print(pares)
+# Usando (for) para pares
