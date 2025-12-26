4f3dcae1c9cbbf58455855a255a551ae499719c1
Samuel Luciano <108229765+SamuelL10Br@users.noreply.github.com>
2025-12-26 16:56:19 -0300

    Update aula6.py

--- Python/aula6.py
+++ Python/aula6.py
@@ -11,3 +11,15 @@ print(aluno["curso"])
 # Usando o for para filrar dados em colchete(chave-valor)
 for chave, valor in aluno.items():
     print(chave, ":", valor)
+    
+# Criando dados mais complexos para filtragem e percorrer os dados
+usuarios = [
+    {"nome": "Ana", "idade": 25},
+    {"nome": "Carlos", "idade": 17},
+    {"nome": "João", "idade": 30}
+]
+
+for usuario in usuarios:
+    if usuario["idade"] >= 18:
+        print(usuario["nome"], "é maior de idade")
+
